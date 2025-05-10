1f3164b0c3341e6f70fd8fdf3dabd3466f67adcf
Mystic Muffin <mm.devlogs@gmail.com>
2025-05-09 19:40:20 -0600

    added my images styles and scripts

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const currentYear = new Date().getFullYear();
+  document.getElementById("currentyear").textContent = currentYear;
+
+  const lastModified = document.lastModified;
+  document.getElementById(
+    "lastModified"
+  ).textContent = `Last modified: ${lastModified}`;
+});
